12c48ed7609d50877de10e94e0776c490f5e9f14
douglas <douglasjr1985@gmail.com>
2023-11-13 18:53:41 -0300

    ajutes

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ import json
 from toolkit.get_config import _get_job_config
 from computer.databricks_instance_pool.instance_pool import DatabricksInstancePoolManager 
 
-def _get_job_config(job_name):
+computer/databricks_cluster/resource/databricks_cluster.json
+
+def _get_job_config(path_config):
     try:
-        with open(f'resources/{job_name}.json', 'r') as config_file:
+        with open(f'{path_config}', 'r') as config_file:
             job_config = json.load(config_file)
         return job_config
     except FileNotFoundError:
@@ -16,7 +18,14 @@ def _get_job_config(job_name):
         logging.error(f"An unexpected error occurred: {e}")
     return None
 
-def _
+def find_databricks_instance_pool(text):
+    parts = text.split('/')
+
+    # Procurando pelo segmento desejado
+    for part in parts:
+        if "databricks_instance_pool" in part:
+            return part
+    return None
 
 
 def main():
@@ -27,20 +36,21 @@ def main():
     parser = argparse.ArgumentParser(description='Process modified Jobs.')
     parser.add_argument('--workspace_url', type=str, help='Workspace URL')
     parser.add_argument('--client_secret', type=str, help='Client Secret')
-    parser.add_argument('--filename', type=str, help='File Name')
+    parser.add_argument('--path_config', type=str, help='Path Config')
     args = parser.parse_args()
 
-    logging.info(f'Processing job: {args.filename}') 
+    logging.info(f'Processing job: {args.path_config}') 
 
     workspace_url = args.workspace_url
     client_secret = args.client_secret
-    job_name = args.filename  
+    path_config = args.filename  
 
-    job_config = _get_job_config(job_name)
 
-    if job_config:
+    #path_config = _get_job_config(path_config)
+
+    if path_config:
         job_manager = DatabricksInstancePoolManager(workspace_url, client_secret)
-        job_manager.create_or_replace_job(job_name, job_config)
+        job_manager.create_or_replace_job(job_name, path_config)
     else:
         logging.error('Unable to create or update the job due to previous errors.')
 
