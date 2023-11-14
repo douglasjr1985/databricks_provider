32a8c7f876d992ae527b60b6a0cdad0c08e76320
douglas <douglasjr1985@gmail.com>
2023-11-13 21:08:16 -0300

    ajustes

--- main.py
+++ main.py
@@ -3,34 +3,33 @@ import logging
 import json
 
 from toolkit.get_config import Config
-from toolkit.instance_pool import DatabricksInstancePoolManager
- 
-def main():
-    # Logging configuration
-    logging.basicConfig(filename='log_file.log', level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-    # Argument configuration
+def configure_logging():
+    """Configures the logging settings."""
+    logging.basicConfig(filename='log_file.log', level=logging.INFO, 
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+def parse_arguments():
+    """Parses command-line arguments."""
     parser = argparse.ArgumentParser(description='Process modified Jobs.')
-    parser.add_argument('--workspace_url', type=str, help='Workspace URL')
-    parser.add_argument('--client_secret', type=str, help='Client Secret')
-    parser.add_argument('--path_config', type=str, help='Path Config')
-    args = parser.parse_args()
+    parser.add_argument('--workspace_url', type=str, required=True, help='Workspace URL')
+    parser.add_argument('--client_secret', type=str, required=True, help='Client Secret')
+    parser.add_argument('--path_config', type=str, required=True, help='Path Config')
+    return parser.parse_args()
 
-    logging.info(f'Processing job: {args.path_config}') 
+def main():
+    # Configure logging
+    configure_logging()
 
-    workspace_url = args.workspace_url
-    client_secret = args.client_secret
-    path_config = args.path_config  
-  
-    path = Config(path_config)
-    config = path.get_config()
+    # Parse command-line arguments
+    args = parse_arguments()
 
-    if path.find_databricks() == 'databricks_instance_pool':
-        insta = DatabricksInstancePoolManager(workspace_url=workspace_url, client_secret=client_secret, path_config=config )
-        insta.create_or_edit_instance_pool(pool_name=, pool_config=path_config)
+    # Log the initiation of job processing
+    logging.info(f'Processing job: {args.path_config}')
 
- 
-
+    # Create a Config instance and execute the job
+    config_instance = Config(args.workspace_url, args.client_secret, args.path_config)
+    config_instance.execute()
 
 if __name__ == '__main__':
     main()
--- toolkit/get_config.py
+++ toolkit/get_config.py
@@ -1,27 +1,47 @@
 import json
 import logging
 
+from toolkit.instance_pool import  DatabricksInstancePoolManager
+
 class Config:
 
-    def __ini__(self, path_config):
-        self.path_config = path_config
+    def __init__(self,workspace_url,client_secret, path_config):
+       self.workspace_url = workspace_url
+       self.client_secret = client_secret
+       self.path_config = path_config
 
-    def get_config(self,path_config):
+    def remove_json_extension(self, file_path):
+        """
+        Removes the .json extension from the file path.
+        """
+        if file_path.endswith('.json'):
+            return file_path[:-5]  # Removes '.json'
+        return None
+
+    def load_config(self, file_path):
+        """
+        Loads the configuration from a JSON file.
+        """
         try:
-            with open(f'{path_config}', 'r') as config_file:
-                config = json.load(config_file)
-            return config
+            with open(file_path, 'r') as config_file:
+                return json.load(config_file)
         except FileNotFoundError:
-            logging.error("File not found.")
-        except (IOError, Exception) as e:
-            logging.error(f"An unexpected error occurred: {e}")
+            logging.error("File not found: " + file_path)
+        except json.JSONDecodeError as e:
+            logging.error("JSON decoding error: " + str(e))
+        except Exception as e:
+            logging.error("An unexpected error occurred: " + str(e))
         return None
-
-    def find_databricks(self,path_config):   
-        parts = path_config.split('/')
-
-        # Procurando pelo segmento desejado
-        for part in parts:
-            if "databricks_instance_pool" in part:
-                return part
-        return None
+    
+    def execute(self):
+        """
+        Executes the configuration process and job execution.
+        """
+        file_path_without_extension = self.remove_json_extension(self.path_config)
+        if file_path_without_extension:
+            parts = file_path_without_extension.split('/')
+            config = self.load_config(self.path_config)
+            if parts and len(parts) > 3 and parts[2] == 'databricks_instance_pool':
+                job_manager = DatabricksInstancePoolManager(self.workspace_url, self.client_secret)
+                job_manager.create_or_edit_instance_pool(parts[3], config)
+ 
